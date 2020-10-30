73126d799688867b825465453ef805da330590ba
l.ladu3 <vagrant@homestead>
2020-10-30 15:30:36 +0000

    Add dish migration with factory

---
+++ database/factories/DishFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Dish;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DishFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Dish::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'Italian_description' => $this->faker->text([40]),
+            'English__description' => $this->faker->text([40]),
+            'German_description'=> $this->faker->text([40]),
+            'price'=>$this->faker->numberBetween([10][500]),
+            'type'=>$this->faker->numberBetween([0][8])
+        ];
+    }
+}
