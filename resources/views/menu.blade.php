809853164977e862f26f7c395b1312d3c1e0a853
l.ladu3 <vagrant@homestead>
2020-10-30 14:48:33 +0000

    Some frontend like home page

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,57 @@
+<!-- Nav tabs -->
+<ul class="nav nav-tabs" role="tablist" style="margin:40px 0;">
+    <li role="presentation"><a class="sld-btn-bor" href="#antipasti" aria-controls="messages" role="tab"
+                               data-toggle="tab" style="color:#A77489;">Antipasti</a></li>
+    <li role="presentation" class="active"><a class="sld-btn-bor" href="#primi" aria-controls="home" role="tab"
+                                              data-toggle="tab" style="color:#A77489;">Primi Piatti</a></li>
+    <li role="presentation"><a class="sld-btn-bor" href="#secondi" aria-controls="profile" role="tab" data-toggle="tab"
+                               style="color:#A77489;">Secondi di Mare</a></li>
+
+    <li role="presentation"><a class="sld-btn-bor" href="#secondiT" aria-controls="messages" role="tab"
+                               data-toggle="tab" style="color:#A77489;">Secondi di Terra</a></li>
+    <li role="presentation"><a class="sld-btn-bor" href="#panini" aria-controls="messages" role="tab" data-toggle="tab"
+                               style="color:#A77489;">Panini</a></li>
+    <li role="presentation"><a class="sld-btn-bor" href="#contorni" aria-controls="profile" role="tab" data-toggle="tab"
+                               style="color:#A77489;">Contorni ed Insalate</a></li>
+
+    <li role="presentation"><a class="sld-btn-bor" href="#dolci" aria-controls="messages" role="tab" data-toggle="tab"
+                               style="color:#A77489;">Dolci</a></li>
+    <li role="presentation"><a class="sld-btn-bor" href="#vinib" aria-controls="messages" role="tab" data-toggle="tab"
+                               style="color:#A77489;">Vini bianchi </a></li>
+    <li role="presentation"><a class="sld-btn-bor" href="#vinir" aria-controls="profile" role="tab" data-toggle="tab"
+                               style="color:#A77489;">Vini rossi</a></li>
+</ul>
+
+
+<div role="tabpanel" class="tab-pane fade" id="antipasti">
+
+    <!--menu list-->
+    <div class="col-xs-12 col-sm-6">
+        <div class="menu-section">
+            <h2 class="menu-section-title">Antipasti</h2>
+            <hr>
+            <div class="menu-item">
+                <div class="menu-item-name"> Delicious Dish </div>
+                <div class="menu-item-price"> $35 </div>
+                <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-name"> Delicious Dish </div>
+                <div class="menu-item-price"> $30 </div>
+                <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-name"> Delicious Dish </div>
+                <div class="menu-item-price"> $30 </div>
+                <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+            </div>
+            <div class="menu-item">
+                <div class="menu-item-name"> Delicious Dish </div>
+                <div class="menu-item-price"> $30 </div>
+                <div class="menu-item-description"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, duis sed dapibus leo nec ornare diam. </div>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+</div>
+
